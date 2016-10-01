4d54862328d5f9bacf10f693082bb7f03b0fb5ce
Ian Lee <lee1001@llnl.gov>
2016-09-30 19:55:02 -0700

    Beautified javascript files

--- js/github-dynamic.js
+++ js/github-dynamic.js
@@ -1,5 +1,5 @@
 angular.module('app', [])
-    .controller('gitHubDataController', ['$scope','$http', function($scope,$http) {
+    .controller('gitHubDataController', ['$scope', '$http', function($scope, $http) {
         $scope.reposLoaded = false;
         $scope.userLoaded = false;
         $scope.username = "llnl";
@@ -20,10 +20,12 @@ angular.module('app', [])
             'dun/munge',
         ];
 
-        angular.forEach($scope.orgs, function(value, key){
+        angular.forEach($scope.orgs, function(value, key) {
             console.log(value);
-            $http.get("https://api.github.com/users/" + value, {cache: true})
-                .success(function (data) {
+            $http.get("https://api.github.com/users/" + value, {
+                    cache: true
+                })
+                .success(function(data) {
                     $scope.userData = data;
                     console.log(data);
                     loadOrganizationRepos();
@@ -31,17 +33,21 @@ angular.module('app', [])
         });
 
         $scope.repoData = [];
-        var loadOrganizationRepos = function () {
-            $http.get($scope.userData.repos_url + "?per_page=100", {cache: true})
-                .success(function (data) {
+        var loadOrganizationRepos = function() {
+            $http.get($scope.userData.repos_url + "?per_page=100", {
+                    cache: true
+                })
+                .success(function(data) {
                     $scope.repoData = $scope.repoData.concat(data);
                     console.log(data);
                 });
         };
 
-        angular.forEach($scope.repos, function(value, key){
-            $http.get("https://api.github.com/repos/" + value, {cache: true})
-                .success(function (data) {
+        angular.forEach($scope.repos, function(value, key) {
+            $http.get("https://api.github.com/repos/" + value, {
+                    cache: true
+                })
+                .success(function(data) {
                     $scope.repoData = $scope.repoData.concat(data);
                     console.log(data);
                     console.log($scope.userData.repos_url);
@@ -49,4 +55,4 @@ angular.module('app', [])
         });
 
         $scope.predicate = '-stargazers_count';
-}]);
+    }]);
