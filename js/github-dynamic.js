918c3aa9af105d78431641e1c4c963bdfb35c65f
Greg Lee <lee218@llnl.gov>
2017-01-31 16:08:57 -0800

    added pruners organization

--- js/github-dynamic.js
+++ js/github-dynamic.js
@@ -11,6 +11,7 @@ angular.module('app', [])
             'glvis',
             'llnl',
             'mfem',
+            'pruners',
             'rose-compiler',
             'zfsonlinux',
         ];
