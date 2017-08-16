498b671a1ac48fe36872178758dff01dc009cbe0
Laura Weber <weber42@llnl.gov>
2017-08-16 13:29:32 -0700

    Archived old data scripts

---
+++ explore/scripts/archive/build_yearlist.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+yearDict = {}
+
+# Gather all file name data
+print "Checking GitHub data file names with year stamps..."
+for file in os.listdir("../github-data"):
+	if file.endswith(".json"):
+		nameSplit = file.split(".")
+		# Must have format "somePrefix.0000.json"
+		if not nameSplit[0] == "YEARS" and nameSplit[1].isdigit() :
+			prefix = nameSplit[0]
+			yearX = int(nameSplit[1])
+			if not prefix in yearDict.keys():
+				yearDict[prefix] = []
+			yearDict[prefix].append(yearX)
+
+print "Sorting year data..."
+# Remove duplicate years (though shouldn't be possible) and sort list
+for prefix in yearDict.keys():
+	yearList = yearDict[prefix]
+	yearList = list(set(yearList))
+	yearList.sort()
+	yearDict[prefix] = yearList
+
+yearDataString = json.dumps(yearDict)
+datfilepath = "../github-data/YEARS.json"
+
+# Write year list file
+print "Writing file '"+datfilepath+"'"
+with open(datfilepath,"w") as fileout:
+	fileout.write(yearDataString)
+print "Wrote file!"
+
+print "Done!\n"
