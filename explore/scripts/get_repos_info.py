94c41f55290e18b283ada84432ba3c80e17497a4
Laura Weber <weber42@llnl.gov>
2017-08-16 14:16:45 -0700

    Archived queries and data, Added repoInfo data collection

---
+++ explore/scripts/get_repos_info.py
@@ -0,0 +1,116 @@
+import helpers
+import json
+import re
+import time
+
+datfilepath = "../github-data/reposInfo.json"
+allData = {}
+
+# Read input list of organizations of interest
+orglist = helpers.read_input("../inputs/Orgs")
+
+# Read input list of independent repos of interest
+repolist = helpers.read_input("../inputs/Repos")
+
+# Read pretty GraphQL queries
+#	Org repos
+query_in = helpers.read_gql("../queries/org-Repos-Info.gql")
+#	Solo repos
+query_solo_in = helpers.read_gql("../queries/repo-Info.gql")
+
+# Retrieve authorization token
+authhead = helpers.get_gitauth()
+
+# Iterate through orgs of interest
+print "Gathering data across multiple paginated queries..."
+collective = {u'data': {}}
+tab = "    "
+
+for org in orglist:
+	pageNum = 1
+	print "\n'"+org+"'"
+	print tab+"page "+str(pageNum)
+
+	print tab+"Modifying query..."
+	newqueryOrg = re.sub('ORGNAME', org, query_in)
+	newquery = re.sub(' PGCURS', '', newqueryOrg)
+	gitquery = json.dumps({'query': newquery})
+	print tab+"Query ready!"
+
+	# Actual query exchange
+	outObj = helpers.query_github(authhead,gitquery)
+	if outObj["errors"] :
+		print tab+"Could not complete '"+org+"'"
+		collective["data"].pop(org, None)
+		continue
+
+	# Update collective data
+	for repo in outObj["data"]["organization"]["repositories"]["nodes"]:
+		repoKey = repo["nameWithOwner"]
+		collective["data"][repoKey] = repo
+
+	# Paginate if needed
+	hasNext = outObj["data"]["organization"]["repositories"]["pageInfo"]["hasNextPage"]
+	while hasNext :
+		pageNum += 1
+		print tab+"page "+str(pageNum)
+		cursor = outObj["data"]["organization"]["repositories"]["pageInfo"]["endCursor"]
+
+		print tab+"Modifying query..."
+		newquery = re.sub(' PGCURS', ', after:"'+cursor+'"', newqueryOrg)
+		gitquery = json.dumps({'query': newquery})
+		print tab+"Query ready!"
+
+		# Actual query exchange
+		outObj = helpers.query_github(authhead,gitquery)
+		if outObj["errors"] :
+			print tab+"Could not complete '"+org+"'"
+			collective["data"].pop(org, None)
+			continue
+
+		# Update collective data
+		for repo in outObj["data"]["organization"]["repositories"]["nodes"]:
+			repoKey = repo["nameWithOwner"]
+			collective["data"][repoKey] = repo
+		hasNext = outObj["data"]["organization"]["repositories"]["pageInfo"]["hasNextPage"]
+
+	print "'"+org+"' Done!"
+
+print "\nCollective data gathering Part1of2 complete!"
+
+# Iterate through independent repos
+print "Adding independent repos..."
+print "Gathering data across multiple queries..."
+for repo in repolist :
+
+	r = repo.split("/")
+	print tab+"Modifying query..."
+	newquery = re.sub('OWNNAME', r[0], query_solo_in)
+	newquery = re.sub('REPONAME', r[1], newquery)
+	gitquery = json.dumps({'query': newquery})
+	print tab+"Query ready!"
+
+	# Actual query exchange
+	outObj = helpers.query_github(authhead,gitquery)
+	if outObj["errors"] :
+		print tab+"Could not complete '"+repo+"'"
+		collective["data"].pop(repo, None)
+		continue
+
+	# Update collective data
+	repoKey = outObj["data"]["repository"]["nameWithOwner"]
+	collective["data"][repoKey] = outObj["data"]["repository"]
+
+print "\nCollective data gathering Part2of2 complete!"
+
+# Combine new data with existing data
+allData["data"] = collective["data"]
+allDataString = json.dumps(allData)
+
+# Write output file
+print "\nWriting file '"+datfilepath+"'"
+with open(datfilepath,"w") as fileout:
+	fileout.write(allDataString)
+print "Wrote file!"
+
+print "\nDone!\n"
