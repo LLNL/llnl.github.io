5e2d1cda6065327d0f60053c0eff4cfe89adbf47
Laura Weber <weber42@llnl.gov>
2017-08-22 12:28:03 -0700

    MASTER script overhaul - now if one of the sub scripts fails, the master script will stop and log the failure

--- explore/scripts/MASTER.sh
+++ explore/scripts/MASTER.sh
@@ -1,6 +1,32 @@
 # Run this script to refresh all data for today
 
 
+# On exit
+function finish {
+	# Log end time
+	echo "End" >> ../github-data/LAST_MASTER_UPDATE.txt
+	date >> ../github-data/LAST_MASTER_UPDATE.txt
+}
+trap finish EXIT
+
+# Stop and Log for failed scripts
+function errorCheck() {
+	if [ $ret -ne 0 ]; then
+		echo "FAILED - $1"
+		echo "FAILED - $1" >> ../github-data/LAST_MASTER_UPDATE.txt
+		exit
+	fi
+}
+
+# Basic script run procedure
+function runScript() {
+	echo "Run - $1"
+	python $1
+	ret=$?
+	errorCheck "$1"
+}
+
+
 echo "RUNNING MASTER UPDATE SCRIPT"
 
 # Log start time
@@ -9,62 +35,35 @@ echo "Start" >> ../github-data/LAST_MASTER_UPDATE.txt
 date >> ../github-data/LAST_MASTER_UPDATE.txt
 
 
-echo "Run - Cleanup Inputs"
 # RUN THIS FIRST
-python cleanup_inputs.py
+runScript cleanup_inputs.py
 
 
 # --- BASIC DATA ---
-
-echo "Run - Get Repos Info"
 # Required before any other repo scripts (output used as repo list)
-python get_repos_info.py
-
-echo "Run - Get LLNL Members"
+runScript get_repos_info.py
 # Required before any other member scripts (output used as member list)
-python get_llnl_members.py
+runScript get_llnl_members.py
 
 
 # --- EXTERNAL V INTERNAL ---
-
-echo "Run - Get Members External Repos"
-python get_members_extrepos.py
-
-echo "Run - Get Repos External Users"
-python get_repos_extusers.py
+runScript get_members_extrepos.py
+runScript get_repos_extusers.py
 
 
 # --- ADDITIONAL REPO DETAILS ---
-
-echo "Run - Get Repos Licenses"
-python get_repos_licenses.py
-
-echo "Run - Get Repos Languages"
-python get_repos_languages.py
-
-echo "Run - Get Repos Topics"
-python get_repos_topics.py
-
-echo "Run - Get Repos Pulls and Issues"
-python get_repos_pullsissues.py
-
-echo "Run - Get Repos Activity"
-python get_repos_activity.py
+runScript get_repos_licenses.py
+runScript get_repos_languages.py
+runScript get_repos_topics.py
+runScript get_repos_pullsissues.py
+runScript get_repos_activity.py
 
 
 # --- HISTORY FOR ALL TIME ---
+runScript get_repos_creationhistory.py
 
-echo "Run - Get Repos Creation History"
-python get_repos_creationhistory.py
-
-
-echo "Run - Build Year List"
 # RUN THIS LAST, used in case of long term cumulative data
-python build_yearlist.py
+runScript build_yearlist.py
 
-
-# Log end time
-echo "End" >> ../github-data/LAST_MASTER_UPDATE.txt
-date >> ../github-data/LAST_MASTER_UPDATE.txt
 
 echo "MASTER UPDATE COMPLETE"
