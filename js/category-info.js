13b22c1ffc4e1aba2db7ce3bdedf62b14775bb75
angfl97 <flores68@llnl.gov>
2019-06-19 10:11:32 -0700

    cleaned up promises

--- js/category-info.js
+++ js/category-info.js
@@ -1,48 +1,33 @@
 angular.module('app', [])
     .controller('gitHubDataController', ['$scope', '$http', '$window', function($scope, $http, $window) {
 
-        var getCategoryInfo = function() {
-            return $http.get("../categories/category_info.json", {
+        var getCategoryInfo =  $http.get("../categories/category_info.json", {
                     cache: true
-                })
-                .then(function (res) {
-                    return res.data;
                 });
-        };
 
-        var getReposTopics = function() {
-            return $http.get("./explore/github-data/labRepos_Topics.json", {
+        var getReposTopics = $http.get("./explore/github-data/labRepos_Topics.json", {
                 cache: true
-            })
-                .then(function(res){
-                    return res.data;
                 });
-        };
 
-        var getReposInfo = function() {
-            return $http.get("./explore/github-data/labReposInfo.json", {
+        var getReposInfo =  $http.get("./explore/github-data/labReposInfo.json", {
                 cache: true
-            })
-                .then(function(res){
-                    return res.data;
                 });
-        };
 
-        var myDataPromise = getCategoryInfo();
-        myDataPromise.then( function(catsObj) {
-            $scope.cats = Object.keys(catsObj.data);
+        getCategoryInfo.then( function(response) {
+            var catsObj = response.data.data;
+            $scope.cats = Object.keys(catsObj);
             $scope.catData = [];
             angular.forEach($scope.cats, function(value, key) {
-                var data = catsObj["data"][value];
+                var data = catsObj[value];
                 $scope.catData.push(data);
             });
 
-            var myRepoDataPromise = getReposTopics();
-            myRepoDataPromise.then(function(reposObj){
-                var allRepos = Object.keys(reposObj.data);
+            getReposTopics.then(function(response){
+                var reposObj = response.data.data;
+                var allRepos = Object.keys(reposObj);
                 $scope.topicRepos = [];
                 angular.forEach(allRepos, function(value, key){
-                    var repoData = reposObj["data"][value];
+                    var repoData = reposObj[value];
                     var topics = [];
                     var containsTopic = false;
                     for(var y in repoData.repositoryTopics.nodes){
@@ -58,15 +43,15 @@ angular.module('app', [])
                     }
                 });
 
-                var myRepoInfoPromise = getReposInfo();
-                myRepoInfoPromise.then(function(reposInfoObj){
-                    for (var i in reposInfoObj.data){
+                getReposInfo.then(function(response){
+                    var reposInfoObj = response.data.data;
+                    for (var i in reposInfoObj){
                         // console.log("repo name: "+ reposInfoObj.data[i].name);
                         for (var j in $scope.topicRepos){
-                            if(reposInfoObj.data[i].nameWithOwner == $scope.topicRepos[j].nameWithOwner){
-                                $scope.topicRepos[j]["name"]= reposInfoObj.data[i].name;
-                                $scope.topicRepos[j]["gitUrl"]= reposInfoObj.data[i].url;
-                                $scope.topicRepos[j]["homepageUrl"]= reposInfoObj.data[i].homepageUrl;
+                            if(reposInfoObj[i].nameWithOwner == $scope.topicRepos[j].nameWithOwner){
+                                $scope.topicRepos[j]["name"]= reposInfoObj[i].name;
+                                $scope.topicRepos[j]["gitUrl"]= reposInfoObj[i].url;
+                                $scope.topicRepos[j]["homepageUrl"]= reposInfoObj[i].homepageUrl;
                             }
                         }
                     }
@@ -75,13 +60,6 @@ angular.module('app', [])
                 $scope.repoHref = function(nametag) {
                     $window.location.href = '../repo#'+nametag;
                 };
-
             });
-
         });
-
-        
-
-       
-
     }]);
