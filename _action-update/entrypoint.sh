791c3a6e1aa02cdf1f0a99052ffacc4ab0b4bfce
Laura Weber <weber42@llnl.gov>
2019-07-19 17:24:39 -0700

    More functionality, Work in progress

--- _action-update/entrypoint.sh
+++ _action-update/entrypoint.sh
@@ -1,3 +1,28 @@
 #!/bin/sh -l
 
-sh -c "echo 'Hello World'"
+set -eu
+. /opt/venv/bin/activate
+
+# Get latest copy of repository
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+git clone --depth 1 https://github.com/LLNL/llnl.github.io.git
+cd llnl.github.io
+REPO_ROOT=$(pwd)
+
+# Checkout new branch
+git checkout $DATA_BRANCHNAME || git checkout -b $DATA_BRANCHNAME
+git merge --ff-only master
+
+# Run MASTER script
+cd $REPO_ROOT/_explore/scripts
+./MASTER.sh
+
+# Commit update
+cd $REPO_ROOT
+TODAY_STAMP=$(date "+%Y-%m-%d")
+git add -A .
+git commit -m "$TODAY_STAMP Auto Data Update"
+
+# Push update
+# git push --set-upstream origin $DATA_BRANCHNAME
