75aef03457dba1c12396b5cc0de4f5df685c3d72
Laura Weber <weber42@llnl.gov>
2019-09-17 12:18:00 -0700

    GitHub Action update. Use authentication in clone URL. Updated to latest hub version.

--- _action-update/entrypoint.sh
+++ _action-update/entrypoint.sh
@@ -13,13 +13,12 @@ DATA_TIMESTAMP=$(date "+%Y-%m-%d-%H")
 CLONE_CUTOFF=$(date "+%Y-%m-%d" -d "7 days ago")
 
 # Configure git + hub
-export GITHUB_USER=$BOT_USER
 git config --global user.name "${BOT_USER}"
 git config --global user.email "${BOT_USER}@users.noreply.github.com"
 git config --global hub.protocol https
 
 # Get latest copy of repository
-git clone --shallow-since=$CLONE_CUTOFF --no-single-branch https://github.com/LLNL/llnl.github.io.git
+git clone --shallow-since=$CLONE_CUTOFF --no-single-branch "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/LLNL/llnl.github.io.git"
 cd llnl.github.io
 REPO_ROOT=$(pwd)
 
@@ -33,12 +32,11 @@ cd $REPO_ROOT/_explore/scripts
 
 # Commit update
 cd $REPO_ROOT
-git config --list
 git add -A .
 git commit -m "${DATA_TIMESTAMP} Data Update by ${BOT_USER}"
 
 # Push update
-git -c http.extraheader="AUTHORIZATION: basic ${GITHUB_TOKEN}" push --set-upstream origin $BRANCH_NAME
+git push origin $BRANCH_NAME
 
 # Create pull request, or list existing
 hub pull-request --no-edit --message "Data Update by ${BOT_USER}" || hub pr list --state open --head $BRANCH_NAME
