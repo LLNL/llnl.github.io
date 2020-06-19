d27800c7ef31ca10de7b9008533574fa32a6faf0
Epperly <epperly5@llnl.gov>
2020-06-19 09:16:06 -0700

    Angled Text to Avoid Crowding

--- js/explore/scatter_repoIssues.js
+++ js/explore/scatter_repoIssues.js
@@ -149,6 +149,13 @@ function draw_scatter_repoIssues(areaID) {
             })
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide);
+
+        // Angle the axis text
+        chart
+            .select('.x.axis')
+            .selectAll('text')
+            .attr('transform', 'rotate(15)')
+            .attr('text-anchor', 'start');
     }
 
     // Sort array of dictionaries by names.length
