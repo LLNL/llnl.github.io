14ae5e42d930f19101d26ed39db2fda1eb867469
DarkFireDrago <darkfiredrago@gmail.com>
2020-07-14 15:22:49 -0700

    Added legend, title, etc.

--- js/explore/force_graph.js
+++ js/explore/force_graph.js
@@ -1,40 +1,19 @@
 function draw_force_graph(areaID) {
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    const n = 50;
-    const nodeArray = [];
-    const linkArray = [];
-    for (var i = 0; i < n; i++) {
-        nodeArray.push({ id: i });
-        nodeArray.push({ id: n + i })
-        /*if (i + 1 < n) {
-            linkArray.push({ source: i, target: i + 1, value: 1 });
-        } else {
-            linkArray.push({ source: i, target: 0, value: 1 });
-        }*/
-    }
-    for (var i = 0; i < n; i++) {
-        let rand = getRandomInt(n);
-        linkArray.push({ source: i, target: n + rand, value: 1 });
-        rand = getRandomInt(n);
-        linkArray.push({ source: i, target: n + rand, value: 1 });
-    }
-    
-    const data = { nodes: nodeArray, links: linkArray };
-
-    console.debug(data);
-
-    drawGraph(data, areaID);
+    var url = ghDataDir + '/labRepos_Dependencies.json';
+    var files = [url];
+    Promise.all(files.map(url => d3.json(url))).then(values => drawGraph(reformatData(values[0]), areaID));
 
     function drawGraph(data, areaID) {
-        const graphHeader = 'LLNL Orgs';
+        const graphHeader = 'LLNL Dependencies';
 
-        const margin = { top: stdMargin.top / 2, right: stdMargin.right / 2, bottom: stdMargin.bottom / 2, left: stdMargin.left / 2 },
-            width = stdTotalWidth - margin.left - margin.right,
-            height = stdTotalHeight - margin.top - margin.bottom;
+        const margin = { top: stdMargin.top, right: stdMargin.right / 2, bottom: stdMargin.bottom / 2, left: stdMargin.left / 2 },
+            width = stdTotalWidth * 2 - margin.left - margin.right,
+            height = stdTotalHeight * 2 - margin.top - margin.bottom;
+        const legendRectSize = 15,
+            legendSpacing = 4;
+
+        const colors = ['#008080','#880808','#60E7AE'];
 
         const chart = d3
             .select('.' + areaID)
@@ -44,6 +23,8 @@ function draw_force_graph(areaID) {
 
         const nodes = data.nodes;
         const links = data.links;
+        console.debug(nodes);
+        console.debug(links);
 
         function weight(x) {
             const b = 0.15;
@@ -53,20 +34,35 @@ function draw_force_graph(areaID) {
         }
 
         const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id).distance(d => 31 - 30 * weight(d.value)))
-            .force('charge', d3.forceManyBody())
-            .force('x', d3.forceX())
-            .force('y', d3.forceY());
+            .force('link', d3.forceLink(links).id(d => d.id).distance(d => 35 - 30 * weight(d.value)))
+            .force('charge', d3.forceManyBody().strength(() => -20))
+            .force('x', d3.forceX().strength(() => 0.14))
+            .force('y', d3.forceY().strength(() => 0.14));
+
+        chart
+            .append('text')
+            .attr('class', 'graphtitle')
+            .attr('x', 0)
+            .attr('y', 0 - (height / 2 - margin.top / 3))
+            .attr('text-anchor', 'middle')
+            .text(graphHeader);
+
+        chart.append('circle')
+            .attr('cx', 0)
+            .attr('cy', 0)
+            .attr('r', (Math.min(width - margin.left - margin.right, height - margin.top - margin.bottom) + 4) / 2)
+            .attr('fill', '#FFFFFF')
+            .attr('stroke', '#000000');
 
         const link = chart
-            .append("g")
-                .attr("stroke", "#999")
-                .attr("stroke-opacity", 0.6)
-            .selectAll("line")
+            .append('g')
+                .attr('stroke', '#999')
+                .attr('stroke-opacity', 0.6)
+            .selectAll('line')
             .data(links)
             .enter()
-                .append("line")
-                .attr("stroke-width", d => (100 - d.value) / 50);
+                .append('line')
+                .attr('stroke-width', d => (100 - d.value) / 50);
 
         const node = chart
             .append('g')
@@ -78,25 +74,98 @@ function draw_force_graph(areaID) {
             .append('circle')
                 .attr('r', 5)
                 .attr('fill', d => {
-                    if (d.id >= n) {
-                        return '#008080';
+                    if (d.notPackage && !d.package) {
+                        return colors[0];
+                    } else if (!d.notPackage && d.package) {
+                        return colors[1];
                     } else {
-                        return '#880808';
+                        return colors[2];
                     }
                 });
-
-        simulation.on("tick", () => {
-            link
-                .attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
-
-            node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
+        
+        const labels = ['LLNL Repositories with Dependencies', 'External Packages', 'Internal Packages'];
+        const legendMap = [];
+        colors.forEach((d, i) => {
+            legendMap.push({ text: labels[i], color: d });
         });
 
-        return chart.node();
+        console.debug(legendMap);
+
+        const legend = chart
+            .append('g')
+            .attr('id', 'foo')
+            .selectAll('g')
+            .data(legendMap)
+                .join('g')
+                .attr('class', 'legend')
+                .attr('transform', (d, i) => {
+                    console.debug(d);
+                    const legendHeight = legendRectSize + legendSpacing;
+                    const offset = (legendHeight * colors.length) / 2;
+                    const horizontal = 0 - width / 2;
+                    const vertical = i * legendHeight - height / 2;
+                    return `translate(${horizontal}, ${vertical})`;
+                });
+        
+        legend
+            .append('rect')
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)
+            .style('fill', d => {
+                return d.color;
+            })
+            .style('stroke', d => {
+                return d.color;
+            });
+
+        legend
+            .append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', legendRectSize - legendSpacing)
+            .text(d => {
+                return d.text;
+            })
+            .attr('text-anchor', 'start');
+
+        node.append('title').text(d => d.id);
+        link.append('title').text(d => `${d.source.name} : ${d.target.name}`);
+
+        simulation.on('tick', () => {
+            link
+                .attr('x1', d => d.source.x)
+                .attr('y1', d => d.source.y)
+                .attr('x2', d => d.target.x)
+                .attr('y2', d => d.target.y);
+
+            node
+                .attr('cx', d => d.x)
+                .attr('cy', d => d.y);
+        });
+    }
+
+    function reformatData(obj) {
+        const nodes = [];
+        const links = [];
+        for (var repo in obj['data']) {
+            if (!nodes.some(d => d.id == repo)) {
+                nodes.push({ name: repo.split('/')[1], id: repo, package: false, notPackage: true });
+            } else {
+                nodes[nodes.findIndex(d => d.id == repo)].notPackage = true;
+            }
+            for (var manifest of obj['data'][repo]['dependencyGraphManifests']['nodes']) {
+                for (var node of manifest['dependencies']['nodes']) {
+                    if (node['repository'] === null) {
+                        continue;
+                    }
+                    if (!nodes.some(d => d.id == node['repository']['nameWithOwner'])) {
+                        nodes.push({ name: node['repository']['name'], id: node['repository']['nameWithOwner'], package: true, notPackage: false });
+                    } else {
+                        nodes[nodes.findIndex(d => d.id == node['repository']['nameWithOwner'])].package = true;
+                    }
+                    links.push({ source: repo, target: node['repository']['nameWithOwner'], value: 1 });
+                }
+            }
+        }
+        return { nodes: nodes.filter(d => links.some(o => d.id == o.source || d.id == o.target)), links: links };
     }
 }
