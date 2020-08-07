b23a0c6492dad617b211aa7c9db6db5ea8139bbe
DarkFireDrago <darkfiredrago@gmail.com>
2020-08-07 09:33:59 -0700

    Added Repo List

--- js/explore/largeRepos/cluster_repoSize.js
+++ js/explore/largeRepos/cluster_repoSize.js
@@ -9,7 +9,7 @@ function draw_cluster(areaID) {
     });
 
     function drawGraph(data, areaID) {
-        const graphHeader = 'Repos Popularity by Stars';
+        const graphHeader = 'Repo\'s Popularity by Stars';
 
         const margin = { top: stdMargin.top, right: stdMargin.right, bottom: stdMargin.bottom, left: stdMargin.left },
             width = stdTotalWidth * 2 - margin.left - margin.right,
---
+++ js/explore/largeRepos/list_popularRepos.js
@@ -0,0 +1,86 @@
+/* Creates line graph visualization for webpage */
+function draw_popularRepos(areaID, columns=2) {
+    var data = reformatData(mostPopularRepositories);
+    drawList(data, areaID);
+
+    function drawList(data, areaID) {
+        const graphHeader = 'Repo Popularity by Stars';
+
+        const rowSpacing = 5,
+            columnSpacing = 150,
+            fontSize = 14;
+        const margin = { top: stdMargin.top, right: stdMargin.right, bottom: stdMargin.bottom, left: stdMargin.left },
+            width = stdTotalWidth,
+            height = (fontSize + rowSpacing) * data[0].entries.length;
+
+        const columnSize = width / data.length - columnSpacing * (data.length - 1);
+
+        const columnSizeTotal = columnSize * data.length + columnSpacing * (data.length - 1);
+
+        const centering = (width - columnSizeTotal) / 2;
+        
+        let n = 0;
+        const textData = data.flatMap(d => d.entries.map(o => {
+            n++;
+            return { entry: o, number: d.number, position: n };
+        }));
+        
+        console.debug(data);
+        console.debug(textData);
+        console.debug(columnSizeTotal);
+
+        const chart = d3
+            .select('.' + areaID)
+                .attr('width', width)
+                .attr('height', height + margin.top)
+                .append('g')
+                    .attr('transform', 'translate(' + centering + ',' + margin.top + ')');
+        
+        const columns = chart
+            .selectAll('g')
+            .data(data)
+            .join('g')
+                .attr('transform', d => `translate(${columnSize * d.number + columnSpacing * d.number},0)`);
+
+        columns.selectAll('text')
+            .data(d => textData.filter(o => o.number == d.number))
+            .join('text')
+                .attr('font-size', fontSize)
+                .attr('y', (d, i) => (fontSize + rowSpacing) * i)
+                .text(d => `#${d.position} ${d.entry.owner}/${d.entry.name}`);
+
+        chart
+            .append('g')
+                .append('text')
+                    .attr('class', 'graphtitle')
+                    .attr('x', width / 2 - centering)
+                    .attr('y', 0 - margin.top / 2)
+                    .attr('text-anchor', 'middle')
+                    .text(graphHeader);
+
+        
+    }
+
+    // Turn json obj into desired working data
+    function reformatData(obj) {
+        const data = [];
+        const minColumnSize = Math.floor(obj.length/columns);
+        const numLargerColumns = obj.length - columns * minColumnSize;
+
+        console.debug('Here!');
+
+        var n = 0;
+        for (var i = 0; i < columns; i++) {
+            data[i] = { entries: [], number: i };
+            var cap = (i < numLargerColumns) ? minColumnSize + 1 : minColumnSize;
+            for (var j = 0; j < cap; j++) {
+                data[i]['entries'].push(obj[n]);
+                n++;
+                console.debug(n);
+            }
+        }
+        console.debug(data);
+
+        return data;
+    }
+}
