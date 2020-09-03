56c6c7c6c1e4dcec5bfa95a935604f5502a46526
DarkFireDrago <darkfiredrago@gmail.com>
2020-09-03 11:46:19 -0700

    Changed color scheme

--- js/explore/pie_language.js
+++ js/explore/pie_language.js
@@ -58,7 +58,9 @@ function draw_pie_languages(areaID, repoNameWOwner) {
             })
             .sort(null);
 
-        const colors = d3.scaleSequential(d3.interpolate('lightblue', 'white')).domain([0, data.length]);
+        const colors = d3.scaleOrdinal()
+            .domain([0, data.length])
+            .range(["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"]);
 
         const pathGroup = chart.append('g');
 
