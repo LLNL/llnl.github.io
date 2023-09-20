7b93068aa631604ec76af35eae04c19c16767438
Laura Weber <weber42@llnl.gov>
2023-09-20 11:08:37 -0700

    Adjust update script limits, schedule

--- _visualize/scripts/get_repos_activitylines.py
+++ _visualize/scripts/get_repos_activitylines.py
@@ -21,7 +21,7 @@ dataCollector = qm.DataManager(datfilepath, False)
 dataCollector.data = {"data": {}}
 
 # Initialize query manager
-queryMan = qm.GitHubQueryManager(maxRetry=10, retryDelay=2)
+queryMan = qm.GitHubQueryManager(maxRetry=15, retryDelay=2)
 
 # Set cutoff timestamp
 cutoffStamp = int((datetime.now() - relativedelta(years=1)).timestamp())
