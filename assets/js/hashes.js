6e7cc5ac77a65c118e7baf75ffbeb354b733be3b
Douglas Fenstermacher <douglas.fenstermacher@gmail.com>
2024-03-28 19:14:25 -0400

    Apply style guide requirements
    Co-authored-by: Laura Weber <LRWeber@users.noreply.github.com>

--- assets/js/hashes.js
+++ assets/js/hashes.js
@@ -1,36 +1,36 @@
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
     setupHashTrigger();
     setupHashUpdates();
 
     function setupHashTrigger() {
         if (!window.location.hash) {
-          return;
+            return;
         }
         const element = document.querySelector(window.location.hash);
         if (!element) {
-          return; 
+            return;
         }
         console.log(element);
         if (element.className.indexOf('accordion-collapse') > -1) {
-            new bootstrap.Collapse(element, { show: true} );
+            new bootstrap.Collapse(element, { show: true });
         }
     }
 
     function setupHashUpdates() {
-      const accordionElements = document.querySelectorAll('.accordion');
-      accordionElements.forEach(function(element){
-        element.addEventListener('show.bs.collapse', function(event) {
-          const hash = '#' + event.target.id;
-          window.history.replaceState('', '', hash);
-        });
+        const accordionElements = document.querySelectorAll('.accordion');
+        accordionElements.forEach(function(element) {
+            element.addEventListener('show.bs.collapse', function(event) {
+                const hash = '#' + event.target.id;
+                window.history.replaceState('', '', hash);
+            });
 
-        element.addEventListener('hide.bs.collapse', function(event) {
-          const hash = '#' + event.target.id;
-          if (window.location.hash == hash) {
-            window.history.replaceState('', '', window.location.pathname + window.location.search);
-          }
-        });
+            element.addEventListener('hide.bs.collapse', function(event) {
+                const hash = '#' + event.target.id;
+                if (window.location.hash == hash) {
+                    window.history.replaceState('', '', window.location.pathname + window.location.search);
+                }
+            });
 
-      });
+        });
     }
 });
